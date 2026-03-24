dc790f9e4866b6e7f910e929add7207ccb294127
haarli <markus.haarlaender@maxit.mpg.de>
2026-03-24 16:42:53 +0100

    Update environment configuration for QA deployment

--- src/environments/environment.qa-pure.ts
+++ src/environments/environment.qa-pure.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  label: "test",
-  cone_instance_uri: "https://gui.inge.mpdl.mpg.de/cone",
-  inge_rest_uri: "https://gui.inge.mpdl.mpg.de/rest",
-  inge_uri: "https://gui.inge.mpdl.mpg.de",
+  label: "qa",
+  cone_instance_uri: "https://qa.pure.mpdl.mpg.de/cone",
+  inge_rest_uri: "https://qa.pure.mpdl.mpg.de/rest",
+  inge_uri: "https://qa.pure.mpdl.mpg.de",
   pure_blog_feed_url: "https://blog.pure.mpg.de/json1",
   matomo_enabled: true,
   matomo_site_url: "https://analytics.mpdl.mpg.de",
